7a9138fc9c64cedfd91e4551e747b62c8e0ae60f
Daniel Gonzalez <dgonzalez93@gmail.com>
2018-09-25 21:24:03 -0500

    Added Exercise 4 and updated index

--- exer3.js
+++ exer3.js
@@ -7,20 +7,3 @@ function hover_image(){
 function unhover_image(){
   document.getElementById('bub_pic').src = norm_img;
 }
-function prev(){
-  if(curr == 0){
-    curr = img_arr.length - 1;
-  }else{
-    curr -= 1;
-  }
-  document.getElementById('mycatpic').src = img_arr[curr];
-}
-
-function next(){
-  if(curr == img_arr.length - 1){
-    curr = 0;
-  }else{
-    curr += 1;
-  }
-  document.getElementById('mycatpic').src = img_arr[curr];
-}
