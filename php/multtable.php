2f050f0b9f455a3e113e51b54bcde4e88d492e61
Daniel Gonzalez <dgonzalez93@gmail.com>
2018-10-02 23:14:24 -0500

    Fixed randomly non-functioning exercise 1

--- php/multtable.php
+++ php/multtable.php
@@ -1,20 +1,18 @@
 <?php
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
-//Inside myfirstprogram.php
-function prod($x, $y) {
-$z = $x * $y;
-return $z;
+function prod($x, $y){
+  $z = $x * $y;
+  return $z;
 }
-echo "<table><td></td><tr><td> </td>";
+
+echo "<table><tr><td></td>";
 for($k = 1; $k < 101; $k++){
-  echo "<td>",$k,"</td>";
+  echo ("<td>$k</td>");
 }
-echo "</tr>";
-for($i = 1; $i < 101; $i++){
-  echo "<td>",$i,"</td>";
+echo ("</tr>");
+for($k = 1; $k < 101; $k++){
+  echo "<td>$k</td>";
   for($j = 1; $j < 101; $j++){
-    echo "<td>",prod($i, $j),"</td>";
+    echo "<td>" . prod($k, $j) . "</td>";
   }
   echo "</tr>";
 }
---
+++ php/mysecondprogram.php
@@ -0,0 +1,25 @@
+<?php
+/*
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+*/
+function prod($x, $y) {
+  $z = $x * $y;
+  return $z;
+}
+
+echo ("<table><td></td><tr><td> </td>");
+
+for($k = 1; $k < 101; $k++){
+  echo ("<td>$k</td>");
+}
+echo ("</tr>");
+for($i = 1; $i < 101; $i++){
+  echo ("<td>",$i,"</td>");
+  for($j = 1; $j < 101; $j++){
+    echo ("<td>" . prod($i, $j) . "</td>");
+  }
+  echo ("</tr>");
+}
+echo ("</table>");
+?>
